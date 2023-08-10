ef59441f8eee2b6f924da5888ecba25005c88ca5
Joshua Gawenda <joshua.gawenda@gmail.com>
2023-08-10 21:15:20 +0200

    Setup playwright

--- next.config.mjs
+++ next.config.mjs
@@ -2,22 +2,31 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.mjs");
+await import('./src/env.mjs')
 
-/** @type {import("next").NextConfig} */
+/** @type {import('next').NextConfig} */
 const config = {
-  reactStrictMode: true,
+    reactStrictMode: true,
 
-  /**
-   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-   * out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-};
+    /**
+     * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
+     * out.
+     *
+     * @see https://github.com/vercel/next.js/issues/41980
+     */
+    i18n: {
+        locales: ['en'],
+        defaultLocale: 'en',
+    },
 
-export default config;
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**',
+            },
+        ],
+    },
+}
+
+export default config
